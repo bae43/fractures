c35a240c073c0dbfd416a1a8a0868a324c3b8afe
bae43 <bae43@cornell.edu>
2015-04-22 15:51:37 -0400

    added some fracture templates

---
+++ rsc/fracture_templates/simple-plane.js
@@ -0,0 +1,42 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.63 Exporter",
+		"vertices"      : 4,
+		"faces"         : 1,
+		"normals"       : 1,
+		"colors"        : 0,
+		"uvs"           : [],
+		"materials"     : 0,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 1.000000,
+
+	"materials" : [],
+
+	"vertices" : [1,4.37114e-08,1,-1,4.37114e-08,1,1,-4.37114e-08,-1,-1,-4.37114e-08,-1],
+
+	"morphTargets" : [],
+
+	"normals" : [0,1,0],
+
+	"colors" : [],
+
+	"uvs" : [],
+
+	"faces" : [33,1,0,2,3,0,0,0,0],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+
+
+}
