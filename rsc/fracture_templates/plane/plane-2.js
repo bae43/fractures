f1bda4a34b3ecf1daffa75d0abe404cbe112006f
bae43 <bae43@cornell.edu>
2015-05-17 22:32:08 -0400

    multiple cut support added

--- rsc/fracture_templates/plane/plane-2.js
+++ rsc/fracture_templates/plane/plane-2.js
@@ -4,9 +4,9 @@
 	{
 		"formatVersion" : 3.1,
 		"generatedBy"   : "Blender 2.63 Exporter",
-		"vertices"      : 4,
-		"faces"         : 2,
-		"normals"       : 3,
+		"vertices"      : 3,
+		"faces"         : 1,
+		"normals"       : 1,
 		"colors"        : 0,
 		"uvs"           : [],
 		"materials"     : 0,
@@ -18,17 +18,17 @@
 
 	"materials" : [],
 
-	"vertices" : [-1,0,-9.03991e-08,1,0,6.05967e-08,-1,-0.5,-0.866026,-1,-0.5,0.866025],
+	"vertices" : [-1,4.37114e-08,1,1,-4.37114e-08,-1,-1,-4.37114e-08,-1],
 
 	"morphTargets" : [],
 
-	"normals" : [0,-1,0,0,-0.866024,0.499985,0,-0.866024,-0.499985],
+	"normals" : [0,-1,0],
 
 	"colors" : [],
 
 	"uvs" : [[]],
 
-	"faces" : [32,1,0,2,0,0,1,32,3,0,1,2,0,0],
+	"faces" : [32,1,0,2,0,0,0],
 
 	"bones" : [],
 
